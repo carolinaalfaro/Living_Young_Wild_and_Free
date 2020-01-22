5b18d29ffa4d46024c6082dc02e9dbbc66b99a63
carolinaalfaro <37843530+carolinaalfaro@users.noreply.github.com>
2020-01-22 13:25:39 -0800

    Add files via upload

---
+++ living.rb
@@ -0,0 +1,42 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 100
+play :f5
+sleep 0.3
+play :e5, sustain: 1
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+play :d5, sustain: 1
+sleep 2
+play :f5
+sleep 0.3
+play :e5, sustain: 1
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :d5
+sleep 1
+play :f5
+sleep 0.3
+play :e5, sustain: 1
+sleep 0.5
+play :d5
+sleep 0.5
+play :b4
+sleep 0.5
+play :d5
+sleep 1
+
+play :e5
+sleep 0.5
+play :d5
+sleep 0.5
+play :e5, sustain: 3
+sleep 1
+play :b4
+sleep 0.5
